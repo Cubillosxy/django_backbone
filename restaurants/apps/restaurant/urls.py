479c3ae3319ae9774d2300a0d1da7a63c179a5e2
EDWIN CUBILLOS <edwin.cubillos@uptc.edu.co>
2017-04-02 21:09:30 -0700

    arreglo

--- restaurants/apps/restaurant/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-from .views import IndexView
-app_name='restaurant'
-
-urlpatterns = [
-    url(r'^$',IndexView.as_view())
-]
