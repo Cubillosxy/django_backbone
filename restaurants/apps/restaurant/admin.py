14d0cf0b7fbda8ea2f15076414f969acc5dfd2c6
EDWIN CUBILLOS <edwin.cubillos@uptc.edu.co>
2017-04-03 04:40:13 -0700

    edwin c

--- restaurants/apps/restaurant/admin.py
+++ restaurants/apps/restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, City, Tip, Restaurant, Payment, Stablishment
+
+
+admin.site.register(City)
+admin.site.register(Category)
+admin.site.register(Tip)
+admin.site.register(Restaurant)
+admin.site.register(Payment)
+admin.site.register(Stablishment)
 
 # Register your models here.
--- restaurants/apps/restaurant/views.py
+++ restaurants/apps/restaurant/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Category,City,Payment,Tip, Stablishment,Restaurant
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name= 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context=super(IndexView,self).get_context_data(**kwargs)
+        context['categories']=Category.objects.all()
+        context['cities'] = City.objects.all()
+        context['payments'] = Payment.objects.all()
+        #context['tips'] = Tip.objects.all()
+        #context['stablishmens'] = Stablishment.objects.all()
+        context['restaurants'] = Restaurant.objects.all()[:5]
+
+        return context
     #return render(request,template_name)
